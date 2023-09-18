12dd3edd40d19c5825e893ecae26c92d6f35fdbb
JosiahMbithi001 <mbithishucks@gmail.com>
2023-09-18 15:02:46 +0300

    Made sme Changes to profile Pages

--- fundis/settings.py
+++ fundis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^e3@4)9efc+f_1n6ejw2^s(=%2iuxjt0td!g1%+63b&-79!m7g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
